de22e3f0140ae0325f10eca294871eb1f58b1825
yishuiwang <yishuiwang163@gmail.com>
2022-07-25 21:10:43 +0800

    实现网络请求

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { nanoid } from 'nanoid';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Main from './components/List/List';
 import './App.css';
 import { Col, List, Row } from 'antd';
+import axios from 'axios';
 
 //Todo数据类型
 export interface Todo {
@@ -15,9 +16,9 @@ export interface Todo {
 
 //初始化数组
 const initialTodos: Todo[] = [
-  { id: '1', name: '吃饭', done: true },
-  { id: '2', name: '睡觉', done: false },
-  { id: '3', name: '敲代码', done: true },
+  // { id: '1', name: '吃饭', done: true },
+  // { id: '2', name: '睡觉', done: false },
+  // { id: '3', name: '敲代码', done: true },
 ];
 
 export default function App() {
@@ -25,22 +26,52 @@ export default function App() {
 
   //增加事项
   const addTodo = (name: string) => {
-    let task: Todo;
-    task = { id: nanoid(), name: name, done: false };
-    let newTodo = [...todos, task];
-    setTodos(newTodo);
+    axios
+      .post('http://localhost:9090/v1/add', {
+        name: name,
+      })
+      .then(
+        (response) => {
+          let task: Todo;
+          task = { id: response.data.ID, name: name, done: false };
+          let newTodo = [...todos, task];
+          setTodos(newTodo);
+        },
+        (error) => {
+          console.log(error);
+        },
+      );
   };
 
   //删除事项
   const deleteTodo = (id: string) => {
-    let newTodo = todos.filter((todo) => {
-      return todo.id !== id;
-    });
-    setTodos(newTodo);
+    axios
+      .post(`http://localhost:9090/v1/delete`, {
+        id: id,
+      })
+      .then(
+        (error) => {
+          console.log(error);
+        },
+      );
   };
 
   //更新事项
   const updateTodo = (id: string, checked: boolean) => {
+    axios
+      .post(`http://localhost:9090/v1/update`, {
+        id: id,
+        status: checked ? 1 : 0,
+      })
+      .then(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        },
+      );
+
     const newTodo = todos.map((todo) => {
       if (todo.id === id) {
         return { ...todo, done: checked };
@@ -52,30 +83,60 @@ export default function App() {
 
   //全选事项
   const checkAll = (done: boolean) => {
+    for (let i = 0; i < todos.length; i++) {
+      updateTodo(todos[i].id, done);
+    }
+
     const newTodo = todos.map((todo) => {
       return { ...todo, done };
     });
     setTodos(newTodo);
   };
 
-  //清空事项
+  //清空已完成事项
   const clearAll = () => {
+    for (let i = 0; i < todos.length; i++) {
+      if (todos[i].done) {
+        deleteTodo(todos[i].id);
+      }
+    }
+    console.log(todos);
+
     const newTodo = todos.filter((todo) => {
       return todo.done !== true;
     });
     setTodos(newTodo);
   };
 
+  useEffect(() => {
+    axios.get('http://localhost:9090/v1/find').then(
+      (response) => {
+        let newTodo: Todo[] = new Array(response.data.length);
+        for (let i = 0; i < response.data.length; i++) {
+          newTodo[i] = {
+            id: response.data[i].ID,
+            name: response.data[i].Name,
+            done: response.data[i].Status === 1 ? true : false,
+          };
+        }
+        setTodos(newTodo);
+      },
+      (error) => {
+        console.log(error);
+      },
+    );
+  }, []);
+
   return (
     <>
       <Row align="middle">
         <Col span={12} offset={6}>
-          <List style={{width:"600px"}}
-            //grid={{gutter:2}}
+          <List
+            style={{ width: '600px' }}
             size="large"
             header={<Header addTodo={addTodo} />}
             footer={
-                <Footer todos={todos} clearAll={clearAll} checkAll={checkAll} />
+              <Footer todos={todos} clearAll={clearAll} checkAll={checkAll} />
             }
             bordered
             dataSource={todos}
@@ -86,7 +147,6 @@ export default function App() {
                 updateTodo={updateTodo}
               />
             )}
-            //renderItem={item => <List.Item>{item.name}</List.Item>}
           />
         </Col>
       </Row>
--- src/components/Item/Item.tsx
+++ src/components/Item/Item.tsx
@@ -1,6 +1,5 @@
-import { Checkbox, List } from 'antd';
+import { Checkbox} from 'antd';
 import React from 'react';
-import { Todo } from '../../App';
 
 interface ItemProps {
   id: string;
@@ -11,61 +10,15 @@ interface ItemProps {
 }
 
 const Item: React.FC<ItemProps> = (Props) => {
-  const myRef = React.createRef<HTMLButtonElement>();
-
-  function handleMouse(flag: boolean) {
-    if (myRef.current) {
-      if (flag === true) {
-        myRef.current.style.display = 'none';
-      } else {
-        myRef.current.style.display = 'none';
-      }
-    }
-  }
-
   function handleChange(id: string) {
     return (event: { target: { checked: boolean } }) => {
       Props.updateTodo(id, event.target.checked);
     };
   }
 
-  function handleDelete(id: string) {
-    if (window.confirm('确定删除吗？')) {
-      Props.deleteTodo(id);
-    }
-    return;
-  }
-
   let { id, name, done } = Props;
 
   return (
-    // <div>
-    //   <div
-    //     onMouseEnter={() => {
-    //       handleMouse(true);
-    //     }}
-    //     onMouseLeave={() => {
-    //       handleMouse(false);
-    //     }}
-    //   >
-    //     &ensp;
-    //     <label>
-    //       <Checkbox checked={done} onChange={handleChange(id)}>
-    //         {name}
-    //       </Checkbox>
-    //     </label>
-    //     <button
-    //       ref={myRef}
-    //       className="btn btn-danger"
-    //       style={{ display: 'none' }}
-    //       onClick={() => {
-    //         handleDelete(id);
-    //       }}
-    //     >
-    //       删除
-    //     </button>
-    //   </div>
-    // </div>
     <h2>
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <Checkbox checked={done} onChange={handleChange(id)}>
